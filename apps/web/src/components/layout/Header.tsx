98587f8c1720140120b6b0fcf9d1048b32df816c
hienjoy <wisdom16no@naver.com>
2026-01-06 18:16:34 +0900

    feat: 헤더에 로고, 프로필 이미지 추가

--- apps/web/src/components/layout/Header.tsx
+++ apps/web/src/components/layout/Header.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import profile from "@/assets/profile.svg";
+
 export default function Header() {
   return (
-    <header className="flex h-24 items-center justify-between bg-sky-200 px-8">
-      <h1 className="text-xl font-bold">쇼미더구조</h1>
+    <header className="border-b border-sky-100 bg-white/80 backdrop-blur-md">
+      <div className="flex items-center justify-between px-6 py-7">
+        <Link href="/" className="flex cursor-pointer items-center gap-3">
+          <img src="/logo.svg" alt="로고" />
+          <h1 className="text-xl font-bold">쇼미더구조</h1>
+        </Link>
+
+        <div className="h-10 w-10 cursor-pointer overflow-hidden rounded-full shadow-md transition-transform hover:scale-105 active:scale-95">
+          <img
+            src={profile.src}
+            alt="프로필 이미지"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
     </header>
   );
 }
