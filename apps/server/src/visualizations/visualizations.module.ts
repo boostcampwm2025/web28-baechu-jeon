6dd3e89c9bdac0b1f0cbd046cbb3a9eba7657676
geunseonkim <guensoen@gmail.com>
2026-01-25 22:13:24 +0900

    feat: module에 PrismaModule imports

--- apps/server/src/visualizations/visualizations.module.ts
+++ apps/server/src/visualizations/visualizations.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VisualizationsController } from './visualizations.controller';
 import { VisualizationsService } from './visualizations.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [VisualizationsController],
   providers: [VisualizationsService],
-  exports: [VisualizationsService],
 })
 export class VisualizationsModule {}
